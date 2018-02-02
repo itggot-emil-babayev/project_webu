7b5871f58fc0dc5a00b185bc0c13ca6ba19db5ba
Emil Babayev <emil.babayev@itggot.se>
2018-02-02 16:02:24 +0100

    Drop-down menu

---
+++ project/site/js/menu.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var element = document.querySelector(".navigation");
+    element.classList.toggle("show");
+
+    element = document.querySelector(".openmenu");
+    element.classList.toggle("show_button");
+}
